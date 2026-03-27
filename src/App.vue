39d9289bb5fd0a2efc8a56b19f488c8b8fb424b2
Nataliia <natalialysenko712@gmail.com>
2026-03-27 07:59:47 +0200

    Task 1: form scaffold + v-model bindings

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,23 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import RegistrationForm from './components/RegistrationForm.vue'
 </script>
 
 <template>
-  <HelloWorld />
+  <div class="app">
+    <h1>Реєстрація</h1>
+    <RegistrationForm />
+  </div>
 </template>
+
+<style scoped>
+.app {
+  max-width: 500px;
+  margin: auto;
+  padding: 20px;
+  font-family: 'Segoe UI', sans-serif;
+}
+h1 {
+  color: #0369a1;
+  text-align: center;
+}
+</style>
---
+++ src/components/RegistrationForm.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { reactive } from 'vue'
+
+const form = reactive({
+  name: '',
+  email: '',
+  password: '',
+  confirmPassword: '',
+  age: null,
+  agree: false
+})
+
+function submitForm() {
+  console.log(form)
+}
+</script>
+
+<template>
+  <form @submit.prevent="submitForm">
+    <input v-model.trim="form.name" placeholder="Ім'я" />
+    <input v-model.trim="form.email" type="email" placeholder="Email" />
+    <input v-model="form.password" type="password" placeholder="Пароль" />
+    <input v-model="form.confirmPassword" type="password" placeholder="Підтвердження" />
+    <input v-model.number="form.age" type="number" placeholder="Вік" />
+
+    <label>
+      <input type="checkbox" v-model="form.agree" />
+      Погоджуюсь
+    </label>
+
+    <button type="submit">Зареєструватися</button>
+  </form>
+</template>
