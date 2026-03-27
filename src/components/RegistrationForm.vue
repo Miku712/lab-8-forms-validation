87ce29c9bf0473acf427ad48db66d56a969a5cea
Nataliia <natalialysenko712@gmail.com>
2026-03-27 08:23:15 +0200

    Task 2: manual validation + errors rendering

--- src/components/RegistrationForm.vue
+++ src/components/RegistrationForm.vue
@@ -10,23 +10,51 @@ const form = reactive({
   agree: false
 })
 
+const errors = reactive({})
+
+function validate() {
+  Object.keys(errors).forEach(key => delete errors[key])
+
+  if (!form.name) errors.name = "Обов'язкове поле"
+  if (!form.email.includes('@')) errors.email = "Невірний email"
+  if (form.password.length < 6) errors.password = "Мінімум 6 символів"
+  if (form.password !== form.confirmPassword)
+    errors.confirmPassword = "Паролі не співпадають"
+  if (form.age < 18) errors.age = "Мінімум 18 років"
+  if (!form.agree) errors.agree = "Потрібно погодитись"
+
+  return Object.keys(errors).length === 0
+}
+
 function submitForm() {
-  console.log(form)
+  if (validate()) {
+    console.log("OK", form)
+  }
 }
 </script>
 
 <template>
   <form @submit.prevent="submitForm">
-    <input v-model.trim="form.name" placeholder="Ім'я" />
-    <input v-model.trim="form.email" type="email" placeholder="Email" />
+    <input v-model="form.name" placeholder="Ім'я" />
+    <p v-if="errors.name">{{ errors.name }}</p>
+
+    <input v-model="form.email" placeholder="Email" />
+    <p v-if="errors.email">{{ errors.email }}</p>
+
     <input v-model="form.password" type="password" placeholder="Пароль" />
+    <p v-if="errors.password">{{ errors.password }}</p>
+
     <input v-model="form.confirmPassword" type="password" placeholder="Підтвердження" />
+    <p v-if="errors.confirmPassword">{{ errors.confirmPassword }}</p>
+
     <input v-model.number="form.age" type="number" placeholder="Вік" />
+    <p v-if="errors.age">{{ errors.age }}</p>
 
     <label>
       <input type="checkbox" v-model="form.agree" />
       Погоджуюсь
     </label>
+    <p v-if="errors.agree">{{ errors.agree }}</p>
 
     <button type="submit">Зареєструватися</button>
   </form>
