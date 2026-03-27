2421e283fbfffff844e6962d22b8040825732b35
Nataliia <natalialysenko712@gmail.com>
2026-03-27 09:05:22 +0200

    Task 4: fixed transitions + full form fields

--- src/components/RegistrationForm.vue
+++ src/components/RegistrationForm.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { reactive, ref, watch } from 'vue'
+import { reactive, ref } from 'vue'
 
 const form = reactive({
   name: '',
@@ -22,57 +22,106 @@ function validate() {
   if (form.password.length < 6) errors.password = "Мінімум 6 символів"
   if (form.password !== form.confirmPassword)
     errors.confirmPassword = "Паролі не співпадають"
-  if (form.age < 18) errors.age = "18+"
+  if (!form.age || form.age < 18) errors.age = "Мінімум 18 років"
   if (!form.agree) errors.agree = "Потрібно погодитись"
 
   return Object.keys(errors).length === 0
 }
 
-watch(form, () => {
-  Object.keys(errors).forEach(k => delete errors[k])
-})
-
 function submitForm() {
+  success.value = false
+
   if (!validate()) return
 
   isSubmitting.value = true
 
   setTimeout(() => {
-    success.value = true
     isSubmitting.value = false
-
-    Object.keys(form).forEach(k => form[k] = k === 'agree' ? false : '')
+    success.value = true
   }, 1500)
 }
 </script>
 
 <template>
-  <form @submit.prevent="submitForm">
+  <form @submit.prevent="submitForm" class="form">
+
     <input v-model="form.name" placeholder="Ім'я" />
-    <p v-if="errors.name">{{ errors.name }}</p>
+    <Transition name="fade">
+      <p v-if="errors.name" class="error">{{ errors.name }}</p>
+    </Transition>
 
     <input v-model="form.email" placeholder="Email" />
-    <p v-if="errors.email">{{ errors.email }}</p>
+    <Transition name="fade">
+      <p v-if="errors.email" class="error">{{ errors.email }}</p>
+    </Transition>
 
     <input v-model="form.password" type="password" placeholder="Пароль" />
-    <p v-if="errors.password">{{ errors.password }}</p>
+    <Transition name="fade">
+      <p v-if="errors.password" class="error">{{ errors.password }}</p>
+    </Transition>
 
     <input v-model="form.confirmPassword" type="password" placeholder="Підтвердження" />
-    <p v-if="errors.confirmPassword">{{ errors.confirmPassword }}</p>
+    <Transition name="fade">
+      <p v-if="errors.confirmPassword" class="error">
+        {{ errors.confirmPassword }}
+      </p>
+    </Transition>
 
     <input v-model.number="form.age" type="number" placeholder="Вік" />
-    <p v-if="errors.age">{{ errors.age }}</p>
+    <Transition name="fade">
+      <p v-if="errors.age" class="error">{{ errors.age }}</p>
+    </Transition>
 
     <label>
       <input type="checkbox" v-model="form.agree" />
-      Погоджуюсь
+      Погоджуюсь з умовами
     </label>
-    <p v-if="errors.agree">{{ errors.agree }}</p>
+    <Transition name="fade">
+      <p v-if="errors.agree" class="error">{{ errors.agree }}</p>
+    </Transition>
 
     <button :disabled="isSubmitting">
       {{ isSubmitting ? "Відправка..." : "Зареєструватися" }}
     </button>
 
-    <p v-if="success">Успішно!</p>
+    <Transition name="fade">
+      <p v-if="success" class="success">
+        Успішна реєстрація!
+      </p>
+    </Transition>
+
   </form>
 </template>
+
+<style scoped>
+.form {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
+
+.error {
+  color: red;
+  font-size: 0.9rem;
+}
+
+.success {
+  color: green;
+  margin-top: 10px;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 0.3s ease;
+}
+
+.fade-enter-from {
+  opacity: 0;
+  transform: translateY(-5px);
+}
+
+.fade-leave-to {
+  opacity: 0;
+  transform: translateY(-5px);
+}
+</style>
